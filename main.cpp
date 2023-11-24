b6eae1c412249f0a2f5d2a052ebcc5e52f1a43e8
spykelionel <social.lionel@gmail.com>
2023-11-24 09:58:34 +0100

    create object of Car

--- main.cpp
+++ main.cpp
@@ -4,6 +4,8 @@
 int main()
 {
     std::cout << "Hello Car race!!!" << std::endl;
+    Car car("Toyota", 11, 1232);
+    car.GetSpeed();
     // Car::SwitchOn();
     // Car::SwitchOff();
     return 0;
