b7256c097cc3af4f3b6bfdab2a75895c401dd880
spykelionel <social.lionel@gmail.com>
2023-12-01 09:30:55 +0100

    create Sedan.h

---
+++ Sedan.h
@@ -0,0 +1,17 @@
+#include "TrafficCar.h"
+
+namespace RecklessDriver
+{
+    class Sedan : public TrafficCar
+    {
+    public:
+        Sedan(int impact, float cash)
+        {
+        }
+
+        void OnCollision(void)
+        {
+        }
+    }
+
+}
